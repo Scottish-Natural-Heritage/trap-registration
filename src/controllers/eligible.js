6098273baccb70e3459b71044bb997932e57a2a7
murrayholmes96 <murray.holmes@nature.scot>
2022-10-06 10:36:56 +0100

    add controllers for new pages
    Issue: Scottish-Natural-Heritage/Licensing#1422

--- src/controllers/eligible.js
+++
@@ -1,9 +0,0 @@
-import {ReturnState} from './_base.js';
-
-const eligibleController = () => {
-  // Much like the start page, the only way out of the eligible page is onwards,
-  // so return success and continue the form.
-  return ReturnState.Positive;
-};
-
-export {eligibleController as default};
