953ba77f5f5ca6fbf9404a9b23b67519ad651677
Paul Cox <paul.cox@nature.scot>
2022-10-20 15:02:29 +0100

    Remove no-longer-used code from details page controller. Issue Licensing 1405

--- src/controllers/details.js
+++ src/controllers/details.js
@@ -14,11 +14,6 @@ const cleanInput = (body) => {
     // copied across if they're in the POST body or are set to undefined if
     // they're missing.
     fullName: body.fullName === undefined ? undefined : body.fullName.trim(),
-    addressLine1: body.addressLine1 === undefined ? undefined : body.addressLine1.trim(),
-    addressLine2: body.addressLine2 === undefined ? undefined : body.addressLine2.trim(),
-    addressTown: body.addressTown === undefined ? undefined : body.addressTown.trim(),
-    addressCounty: body.addressCounty === undefined ? undefined : body.addressCounty.trim(),
-    addressPostcode: body.addressPostcode === undefined ? undefined : body.addressPostcode.trim(),
     phoneNumber: body.phoneNumber === undefined ? undefined : body.phoneNumber.trim(),
     emailAddress:
       body.emailAddress === undefined
@@ -31,11 +26,6 @@ const detailsController = (request) => {
   // Clean up the user's input before we store it in the session.
   const cleanForm = cleanInput(request.body);
   request.session.fullName = cleanForm.fullName;
-  request.session.addressLine1 = cleanForm.addressLine1;
-  request.session.addressLine2 = cleanForm.addressLine2;
-  request.session.addressTown = cleanForm.addressTown;
-  request.session.addressCounty = cleanForm.addressCounty;
-  request.session.addressPostcode = cleanForm.addressPostcode;
   request.session.phoneNumber = cleanForm.phoneNumber;
   request.session.emailAddress = cleanForm.emailAddress;
 
@@ -43,9 +33,6 @@ const detailsController = (request) => {
   request.session.detailsError = false;
   // ...and the specific errors.
   request.session.nameError = false;
-  request.session.addressError = false;
-  request.session.townError = false;
-  request.session.postcodeError = false;
   request.session.phoneError = false;
   request.session.emailError = false;
 
@@ -53,20 +40,6 @@ const detailsController = (request) => {
   if (request.body.fullName === undefined || request.body.fullName.trim() === '') {
     request.session.nameError = true;
   }
-
-  if (request.body.addressLine1 === undefined || request.body.addressLine1.trim() === '') {
-    request.session.addressError = true;
-  }
-
-  if (request.body.addressTown === undefined || request.body.addressTown.trim() === '') {
-    request.session.townError = true;
-  }
-
-  // Call natureScot utils to check validity of postcode
-  request.session.postcodeError =
-    request.body.addressPostcode === undefined
-      ? true
-      : !utils.postalAddress.isaRealUkPostcode(request.body.addressPostcode);
 
   // The smallest, non-local, non-shortcode UK phone number is '08001111'.
   // The longest could be something like 	'+44 (01234) 567 890', but we're not
@@ -92,9 +65,6 @@ const detailsController = (request) => {
   // Check that any of the fields are invalid.
   request.session.detailsError =
     request.session.nameError ||
-    request.session.addressError ||
-    request.session.townError ||
-    request.session.postcodeError ||
     request.session.phoneError ||
     request.session.emailError;
 
@@ -104,30 +74,7 @@ const detailsController = (request) => {
     return ReturnState.Error;
   }
 
-  // Build the address array, ignoring any blank fields.
-  const address = [];
-  if (request.session.addressLine1 !== undefined && request.session.addressLine1.trim() !== '') {
-    address.push(request.session.addressLine1);
-  }
-
-  if (request.session.addressLine2 !== undefined && request.session.addressLine2.trim() !== '') {
-    address.push(request.session.addressLine2);
-  }
-
-  if (request.session.addressTown !== undefined && request.session.addressTown.trim() !== '') {
-    address.push(request.session.addressTown);
-  }
-
-  if (request.session.addressCounty !== undefined && request.session.addressCounty.trim() !== '') {
-    address.push(request.session.addressCounty);
-  }
-
-  if (request.session.addressPostcode !== undefined && request.session.addressPostcode.trim() !== '') {
-    address.push(request.session.addressPostcode);
-  }
-
-  // Create the display versions of the visitors address and contact info.
-  request.session.displayAddress = address.join('<br>');
+  // Create the display version of the visitors contact info.
   request.session.displayContact = `${request.session.phoneNumber}<br>${request.session.emailAddress}`;
 
   // The request passed all our validation, we've stored copies of everything we
