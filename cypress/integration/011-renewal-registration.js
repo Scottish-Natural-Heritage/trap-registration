741440b46030d33f71ced6ae233e2ea7c6b5f0b8
Jen Foster <jennifer.massie@nature.scot>
2024-03-14 10:03:36 +0000

    Updated cypress test. Scottish-Natural-Heritage/Licensing#2063

--- cypress/integration/011-renewal-registration.js
+++ cypress/integration/011-renewal-registration.js
@@ -1,12 +1,12 @@
 describe('Renewal registration page ', function () {
-    it('should prevent access', function () {
-      cy.visit('/renewal-registration', {failOnStatusCode: false});
-      cy.get('h1').should('contain', 'there is a problem with the service');
-    });
+  it('should prevent access', function () {
+    cy.visit('/renewal-registration', {failOnStatusCode: false});
+    cy.get('h1').should('contain', 'there is a problem with the service');
   });
+});
 
-  describe('Address page valid postcode scenario', function () {
-    beforeEach(() => {
+describe('Address page valid postcode scenario', function () {
+  beforeEach(() => {
     cy.visit('/renewal-intro');
 
     cy.get('#main-content form button.naturescot-forward-button').click();
