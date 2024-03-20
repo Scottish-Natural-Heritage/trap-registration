1a2f29359b93ddccb0e3d3651dc4532de9925c0f
Jen Foster <jennifer.massie@nature.scot>
2024-03-20 13:48:28 +0000

    Added content and cypress test for renewal registration. Scottish-Natural-Heritage/Licensing#2065

--- cypress/integration/011-renewal-registration.js
+++ cypress/integration/011-renewal-registration.js
@@ -11,11 +11,57 @@ describe('Move to next page', function () {
 
     cy.get('h1').should('contain', 'Renewal intro');
     cy.get('#main-content form button.naturescot-forward-button').click();
+  });
 
-    cy.get('h1').should('contain', 'Renewal registration number');
+  it('should allow access if the user visits all the pages in order', function () {
+    cy.visit('/renewal-registration');
+    cy.get('h1').should('contain', 'What is the registration number?');
+  });
+
+  it('Valid input followed by continue button should navigate to renewal postcode', () => {
+    cy.visit('/renewal-registration');
+    cy.get('input').type('12345');
     cy.get('#main-content form button.naturescot-forward-button').click();
-
     cy.url().should('include', '/renewal-postcode');
     cy.get('h1').should('contain', 'Renewal postcode');
+  });
+
+  it('Empty input followed by continue button should present an error and ask user to re-enter', () => {
+    cy.visit('/renewal-registration');
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/renewal-registration');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+    cy.get('#renewal-registration-number-error').should('contain', 'Enter the registration number');
+  });
+
+  it('Whitespace input followed by continue button should present an error and ask user to re-enter', () => {
+    cy.visit('/renewal-registration');
+    cy.get('input').type('     ');
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/renewal-registration');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+    cy.get('#renewal-registration-number-error').should('contain', 'Enter the registration number');
+  });
+
+  it('Invalid input followed by continue button should present an error and ask user to re-enter', () => {
+    cy.visit('/renewal-registration');
+    cy.get('input').type('123456');
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/renewal-registration');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+    cy.get('#renewal-registration-number-error').should('contain', 'Enter the registration number');
+  });
+
+  it('Invalid input followed by continue button should present an error and ask user to re-enter', () => {
+    cy.visit('/renewal-registration');
+    cy.get('input').type('abcde');
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/renewal-registration');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+    cy.get('#renewal-registration-number-error').should('contain', 'Enter the registration number');
   });
 });
