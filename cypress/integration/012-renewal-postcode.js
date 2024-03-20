f8c913e98f0b769fb8e8e73387d9866a8a983e3f
Jen Foster <jennifer.massie@nature.scot>
2024-03-20 13:14:46 +0000

    Added content and cypress test for renewal postcode details. Scottish-Natural-Heritage/Licensing#2066

--- cypress/integration/012-renewal-postcode.js
+++ cypress/integration/012-renewal-postcode.js
@@ -14,11 +14,45 @@ describe('Move to next page', function () {
 
     cy.get('h1').should('contain', 'Renewal registration number');
     cy.get('#main-content form button.naturescot-forward-button').click();
+  });
 
-    cy.get('h1').should('contain', 'Renewal postcode');
+  it('should allow access if the user visits all the pages in order', function () {
+    cy.visit('/renewal-postcode');
+    cy.get('h1').should('contain', 'What is your postcode?');
+  });
+
+  it('should display error if the user submits an empty form', function () {
+    cy.visit('/renewal-postcode');
+    cy.get('h1').should('contain', 'What is your postcode?');
+
     cy.get('#main-content form button.naturescot-forward-button').click();
 
-    cy.url().should('include', '/renewal-email-success');
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+
+    cy.get('.govuk-error-summary ul li a').should('contain', 'Enter your postcode');
+  });
+
+  it('should display error if the user submits an invalid postcode', function () {
+    cy.visit('/renewal-postcode');
+    cy.get('h1').should('contain', 'What is your postcode?');
+
+    cy.get('input[type="text"]#renewal-postcode').type('NOTAVALIDPOSTCODE', {delay: 1});
+
+    cy.get('#main-content form button.naturescot-forward-button').click();
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+
+    cy.get('.govuk-error-summary ul li a').should('contain', 'Enter a valid postcode');
+  });
+
+  it('should navigate to address page on valid form submission', function () {
+    cy.visit('/renewal-postcode');
+    cy.get('h1').should('contain', 'What is your postcode?');
+
+    cy.get('input[type="text"]#renewal-postcode').type('IV3 8NW', {delay: 1});
+
+    cy.get('#main-content form button.naturescot-forward-button').click();
+
     cy.get('h1').should('contain', 'Renewal email success');
   });
 });
--- src/controllers/renewal-postcode.js
+++ src/controllers/renewal-postcode.js
@@ -1,6 +1,52 @@
+import utils from 'naturescot-utils';
+import Gazetteer from '../utils/gazetteer.js';
+import config from '../config.js';
 import {ReturnState} from './_base.js';
 
-const renewalPostcodeController = (_request) => {
+const renewalPostcodeController = async (request) => {
+  // Set the errors to false.
+  request.session.renewalPostcodeError = false;
+  request.session.missingRenewalPostcodeError = false;
+  request.session.invalidRenewalPostcodeError = false;
+
+  // Clean the request by trimming leading and trailing whitespace.
+  const renewalPostcode = request.body.renewalPostcode === undefined ? undefined : request.body.renewalPostcode.trim();
+
+  // Check for any errors.
+  request.session.missingRenewalPostcodeError =
+    renewalPostcode === undefined || renewalPostcode === null || renewalPostcode === '';
+  request.session.invalidRenewalPostcodeError =
+    !request.session.missingRenewalPostcodeError && !utils.postalAddress.isaRealUkPostcode(renewalPostcode);
+
+  request.session.renewalPostcodeError =
+    request.session.missingRenewalPostcodeError || request.session.invalidRenewalPostcodeError;
+
+  // If we have any errors return the error state.
+  if (request.session.renewalPostcodeError) {
+    return ReturnState.Error;
+  }
+
+  // No errors so save the renewalPostcode the the request's session.
+  request.session.renewalPostcode = renewalPostcode;
+
+  // If we make it here then make the renewalPostcode lookup call.
+  try {
+    const gazetteerAddresses = await Gazetteer.findAddressesByPostcode(config, renewalPostcode ?? '');
+
+    request.session.uprnAddresses = [];
+
+    request.session.uprnAddresses = gazetteerAddresses.map((address) => {
+      return {
+        value: address.uprn,
+        text: address.summary_address,
+        selected: address.uprn === request.session.uprn
+      };
+    });
+  } catch (error) {
+    console.log('Error finding addresses: ' + error);
+    request.session.uprnAddresses = [{value: 0, text: 'No addresses found.', selected: true}];
+  }
+
   // Proceed to the next page.
   return ReturnState.Positive;
 };
