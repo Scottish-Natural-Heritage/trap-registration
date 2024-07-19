bb52bca289e3f181776696cc113bf0b37ea027f8
Paul Cox <paul.cox@nature.scot>
2024-07-19 08:19:58 +0100

    Add UUID to every request to mitigate against replay attack. Issue https://github.com/Scottish-Natural-Heritage/Deer-Online-Services/issues/971

--- src/controllers/start.js
+++ src/controllers/start.js
@@ -1,3 +1,4 @@
+import {randomUUID} from 'node:crypto';
 import {ReturnState} from './_base.js';
 
 const startController = (request) => {
@@ -5,6 +6,9 @@ const startController = (request) => {
   // this page, we know the user's seen the banner, so we don't need to show it
   // again.
   request.session.seenCookie = true;
+
+  // Create a UUID so we can mitigate the risk of a replay attack.
+  request.session.uuid = randomUUID();
 
   // The only way out of the start page is onwards, so return success and begin
   // the form.
