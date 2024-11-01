a7c2d0a9befa9da6890ac467d0142c32242ff060
Rory Anderson <rory.anderson@nature.scot>
2024-11-01 13:59:29 +0000

    Update tests

--- cypress/e2e/011-renewal-registration.cy.js
+++ cypress/e2e/011-renewal-registration.cy.js
@@ -9,7 +9,7 @@ describe('Move to next page', function () {
   beforeEach(() => {
     cy.visit('/renewal-intro');
 
-    cy.get('h1').should('contain', 'Renew a trap registration');
+    cy.get('h1').should('contain', 'Apply for trap renewals');
     cy.get('#main-content form button.naturescot-forward-button').click();
   });
 
