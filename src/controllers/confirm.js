b6fcd7809008dd6737de84ca7a224f31e0604116
Graham Hotchkiss <graham.hotchkiss@nature.scot>
2024-11-12 09:31:25 +0000

    Handle date as a date as that is how it should come out of the API #2508
    - By adding a date formatter to the existing date utils

--- src/controllers/confirm.js
+++ src/controllers/confirm.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import config from '../config.js';
 import {ReturnState} from './_base.js';
+import {formattedDateString} from '../utils/date-utils.js';
 
 const confirmController = async (request) => {
   // Grab the form as a json object.
@@ -46,7 +47,7 @@ const confirmController = async (request) => {
 
     if (newRegResponse.data) {
       request.session.regNo = `NS-TRP-${String(newRegResponse.data.id).padStart(5, '0')}`;
-      request.session.expiryDate = newRegResponse.data.expiryDate;
+      request.session.expiryDate = formattedDateString(newRegResponse.data.expiryDate);
     } else {
       request.session.alreadyReceivedApplication = true;
     }
--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -12,4 +12,21 @@ const yearsAgo = (years) => {
   return new Date(currentDate.getFullYear() - years, currentDate.getMonth(), currentDate.getDate());
 };
 
-export {monthsFromNow, yearsAgo};
+const formattedDateString = (dateString) => {
+  if (!dateString) {
+    return undefined;
+  }
+
+  try {
+    const fullDate = new Date(dateString);
+    const d = fullDate.getDate();
+    const m = fullDate.getMonth() + 1;
+    const y = fullDate.getFullYear();
+
+    return `${d}/${m}/${y}`;
+  } catch (error) {
+    return undefined;
+  }
+};
+
+export {monthsFromNow, yearsAgo, formattedDateString};
