05c8b37d632601adeb5dfc5ff120be336f0b0d9c
Rory Anderson <rory.anderson@nature.scot>
2024-12-11 12:18:52 +0000

    Fix session issue where data was contaminated and new UUID not being set when renewing another registration

--- src/controllers/renewal-check-answers.js
+++ src/controllers/renewal-check-answers.js
@@ -6,7 +6,7 @@ import {ReturnState} from './_base.js';
 
 const getController = async (request) => {
   // We only want to pull data from the DB into the model once.
-  if (!request.session.populatedModel) {
+  if (!request.session.populatedModel || request.session.registrationId !== request.query.id) {
     const registrationId = request.query.id;
 
     try {
@@ -43,7 +43,7 @@ const getController = async (request) => {
       request.session.registrationId = registrationId;
       request.session.trapId = trapRegistrationData.trapId;
       request.session.populatedModel = true;
-      request.session.renewalUUID ??= randomUUID();
+      request.session.renewalUUID = randomUUID();
     } catch (error) {
       console.log(`Error getting registration ${registrationId}: ` + error);
     }
@@ -100,6 +100,7 @@ const postController = async (request) => {
       request.session.renewedRegistrationId = newRenewalResponse.data.trapId;
     } else {
       request.session.alreadyReceivedRenewal = true;
+      request.session.renewedRegistrationId = undefined;
     }
 
     // We may want to renew another registration, so clear the flag.
