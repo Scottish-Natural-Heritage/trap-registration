bd4a42f86f38b2c0f7eb91565d674835f4ab34bf
ArronChaveAC <125369855+ArronChaveAC@users.noreply.github.com>
2024-12-16 14:21:33 +0000

    Remove files

--- cypress/e2e/015-renewal-success.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Renewal success page ', function () {
-  it('should prevent access', function () {
-    cy.visit('/renewal-success', {failOnStatusCode: false});
-    cy.get('h2').should('contain', 'What happens next');
-  });
-});
