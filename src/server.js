b77e0cc78839606ac18f526e81ba83b864fc7b1d
Paul Cox <paul.cox@nature.scot>
2026-02-16 10:31:20 +0000

    Use global process rather than import. No issue

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 // Load the config.
-import {exit} from 'node:process';
 import config from './config.js';
 import {counterpart100yearToken} from './http-request.js';
 // Load the app.
@@ -8,7 +7,7 @@ import app from './app.js';
 // Run it.
 app.listen(config.port, () => {
   console.log('FAKE CRASH!!!');
-  exit(1);
+  process.exit(1);
   console.log(`Server listening on http://localhost:${config.port}${config.pathPrefix}.`);
   if (process.env.TRR_TEST) {
     console.log(
